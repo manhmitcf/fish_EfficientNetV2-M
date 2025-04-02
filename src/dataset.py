45fda054ff0aa8591575d2aaf41fc49dc337cc0a
manhmitcf <manhmit1221@gmail.com>
2025-04-03 00:23:29 +0700

    ok

--- src/dataset.py
+++ src/dataset.py
@@ -52,13 +52,13 @@ class FishDatasetWithAugmentation(Dataset):
         return image, label
     
 basic_transform = transforms.Compose([
-    transforms.Resize((224, 224), interpolation=transforms.InterpolationMode.LANCZOS),
+    transforms.Resize((480, 480), interpolation=transforms.InterpolationMode.LANCZOS),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
 aug_transform = transforms.Compose([
-    transforms.Resize((224, 224), interpolation=transforms.InterpolationMode.LANCZOS),
+    transforms.Resize((480, 480), interpolation=transforms.InterpolationMode.LANCZOS),
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomRotation(degrees=15),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
